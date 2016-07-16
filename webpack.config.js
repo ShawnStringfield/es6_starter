69a63bf491aaeeb0983f8c9c62f4adda2610c573
Shawn Stringfield <shawn.stringfield@gmail.com>
2016-07-15 20:43:46 -0400

    first push

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname,
+    filename: './src/min/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
